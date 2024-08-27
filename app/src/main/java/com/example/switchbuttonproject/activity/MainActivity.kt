9ad33031605733f01471dfda99942c54d32f9d39
Can Sılacı <mcsilaci@gmail.com>
2024-08-28 00:21:03 +0300

    fragments and menu are created

--- app/src/main/java/com/example/switchbuttonproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/switchbuttonproject/activity/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.example.switchbuttonproject
+package com.example.switchbuttonproject.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.switchbuttonproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
